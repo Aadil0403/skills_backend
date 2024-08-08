3b34581e7ba1945458f6393e2056a9461e53e9f6
Dev Ashrit Behera <devashritcode@gmail.com>
2024-08-08 22:52:46 +0530

    route access allowed

--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -8,7 +8,7 @@ router.post('/signup', userController.signup)
 router.post('/login', userController.login)
 router.get('/dashboard',auth, userController.getUser)
 router.get('/self', auth, userController.getUserEmail)
-router.get('/alluser', auth, userController.getUserName)
+router.get('/alluser', userController.getUserName)
 router.delete('/domain', auth, userController.updateUserDomain)
 
 module.exports = router
