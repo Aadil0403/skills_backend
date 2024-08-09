1c6762b5d27e1fc6c8d51afbff35e55a5459de11
subhamexplore <subhamparidamain@gmail.com>
2024-08-09 15:45:52 +0530

    mail updated

--- src/api/helpers/htmlMail.js
+++ src/api/helpers/htmlMail.js
@@ -1,13 +1,13 @@
 exports.htmlMail = (username) => {
   return `     <div
         class="container p-5"
-        style="background-color:black;color: white;padding: 50px; text-align: justify;line-height: 25px;"
+        style="padding: 50px 0px; text-align: justify;line-height: 25px;"
       >
-        <span style="font-weight:700;">
-          <span style="font-weight:500;">Hey, ${username}</span>
-          <span style="font-weight:400;">You have been registered with Skils++</span>
-          <span style="font-weight:400;">Please tap on the below discord link to join the community and mentorship.</span>
+        <div style="font-weight:700;">
+          <div style="font-weight:500;">Hey, ${username}</div>
+          <div style="font-weight:400;">You have been registered with Skils++</div>
+          <div style="font-weight:400;">Please tap on the below discord link to join the community and mentorship.</div>
           <a href="https://discord.com/invite/qyHgGnmb" target="_blank style="font-weight:400;">https://discord.com/invite/qyHgGnmb</a>
-        </span>
+        </div>
       </div>`;
 };
